d8f2d70a21eb8159d6cbe1e379eabdafee046440
JoseMorenob <jmoren13@ucm.es>
2026-06-23 20:34:21 +0200

    cambio app name y level end

--- Shhh!/Assets/Scripts/GameManager.cs
+++ Shhh!/Assets/Scripts/GameManager.cs
@@ -35,6 +35,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        currentScene = SceneManager.GetActiveScene().name;
         EnsureTelemetrySessionStarted();
     }
    
